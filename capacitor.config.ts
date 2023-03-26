074e4069742b975024b05d668da2e60562fb22e7
Abdulmalik Alsufayran <alive2tinker@Abdulmaliks-MacBook-Pro-2.local>
2023-03-26 11:24:13 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'zain-b2b',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
