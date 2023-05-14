13cf4ff9f98260fbb8bb7b7a4ea1abf126369f13
Abdulmalik Alsufayran <abdulmalik.alsufayra@sa.zain.com>
2023-05-14 18:57:05 +0300

    remove statusbar error

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,20 @@
 </template>
 
 <script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { IonApp, IonRouterOutlet, isPlatform } from '@ionic/vue';
 import { StatusBar, Style } from '@capacitor/status-bar';
 import { onMounted } from 'vue';
 
 onMounted(() => {
-  try{
-    const setStatusBarStyleDark = async () => {
-    await StatusBar.setStyle({ style: Style.Light });
-  };
-    setStatusBarStyleDark();
-  }catch(e){
-    //
+  if (isPlatform('capacitor')) {
+    try {
+      const setStatusBarStyleDark = async () => {
+        await StatusBar.setStyle({ style: Style.Light });
+      };
+      setStatusBarStyleDark();
+    } catch (e) {
+      //
+    }
   }
 })
 </script>
